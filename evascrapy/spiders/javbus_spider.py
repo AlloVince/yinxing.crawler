486b1a6a8e1be18bafdb90bd8242eaee21a006b6
AlloVince <allo.vince@gmail.com>
2019-03-05 14:50:48 +0800

    fix: update spider

--- evascrapy/spiders/javbus_spider.py
+++ evascrapy/spiders/javbus_spider.py
@@ -10,37 +10,48 @@ from evascrapy.items import RawHtmlItem
 import re
 
 domain = os.environ.get('DOMAIN') or 'www.javbus.in'
+domain2 = os.environ.get('DOMAIN2') or 'www.javbus.work'
 
 regex = re.compile(
     r"<script>[\n\s]+var gid = (\d+);[\n\s]+var uc = (\d+);[\n\s]+var img = '(http[\w:\/\.]+)';[\n\s]*</script>", re.S)
+
+magnet = re.compile(r"magnet:\?xt=urn:btih:[a-zA-Z0-9]{40,40}")
 
 
 class JavbusSpider(BaseSpider):
     version = '1.0.0'
     name = 'javbus'
-    allowed_domains = [domain]
+    allowed_domains = [domain, domain2]
     start_urls = [
         'https://%s' % domain,
+        'https://%s' % domain2,
     ]
 
     deep_start_urls = [
         'https://%s' % domain,
+        'https://%s' % domain2,
     ]
 
     rules = (
-        Rule(LinkExtractor(allow='/page/\d+', ), follow=True),
-        Rule(LinkExtractor(allow='/[a-zA-Z]+-\d+', ), follow=True, callback='handle_page'),
+        Rule(LinkExtractor(allow='/page/(1|2|3|4|5|6|7|8|9)', ), follow=True),
+        Rule(LinkExtractor(allow='%s/uncensored' % domain, ), follow=True),
+        Rule(LinkExtractor(allow='%s/uncensored/page/(1|2|3|4|5|6|7|8|9)' % domain, ), follow=True),
+        Rule(LinkExtractor(allow='%s/[^\/]{3,}$' % domain, ), follow=False, callback='handle_page'),
+        Rule(LinkExtractor(allow='%s/[^\/]{3,}$' % domain2, ), follow=False, callback='handle_page'),
     )
 
     deep_rules = (
         Rule(LinkExtractor(allow='/page/\d+', ), follow=True),
-        Rule(LinkExtractor(allow='/[a-zA-Z]+-\d+', ), follow=True, callback='handle_page'),
+        Rule(LinkExtractor(allow='%s/uncensored' % domain, ), follow=True),
+        Rule(LinkExtractor(allow='%s/uncensored/page/\d+' % domain, ), follow=True),
+        Rule(LinkExtractor(allow='%s/[^\/]{3,}$' % domain, ), follow=True, callback='handle_page'),
+        Rule(LinkExtractor(allow='%s/[^\/]{3,}$' % domain2, ), follow=True, callback='handle_page'),
     )
 
     def handle_page(self, response: Response) -> Request:
         match = regex.search(response.text)
         if not match:
-            pass
+            return
 
         request = Request(
             url='https://%s/ajax/uncledatoolsbyajax.php?gid=%s&uc=%s&img=%s' % (
@@ -52,5 +63,7 @@ class JavbusSpider(BaseSpider):
         yield request
 
     def handle_ajax(self, response: Response):
+        if not magnet.search(response.text):
+            return
         return RawHtmlItem(url=response.meta['from_url'], html=response.text, task=self.settings.get('APP_TASK'),
                            version=self.version, timestamp=math.floor(time.time()))
