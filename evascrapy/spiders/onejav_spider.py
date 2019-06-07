a9af9000e9006729dbd46ac7611d4a108d67fd8a
AlloVince <allo.vince@gmail.com>
2019-06-07 20:32:06 +0800

    fix: new spider

--- evascrapy/spiders/onejav_spider.py
+++ evascrapy/spiders/onejav_spider.py
@@ -8,31 +8,30 @@ from evascrapy.base_spider import BaseSpider
 from scrapy.http import Response, Request
 from evascrapy.items import TorrentFileItem
 
-domain = os.environ.get('DOMAIN') or 'pornleech.is'
+domain = os.environ.get('DOMAIN') or 'onejav.com'
 
 
-class PornleechSpider(BaseSpider):
+class OnejavSpider(BaseSpider):
     version = '1.0.0'
-    name = 'pornleech'
+    name = 'onejav'
     allowed_domains = [domain]
     start_urls = [
-        'http://%s/index.php?page=torrents' % domain,
+        'https://%s/new' % domain,
     ]
 
     deep_start_urls = [
-        'http://%s/index.php?page=torrents' % domain,
+        'https://%s/actress/' % domain,
     ]
 
     rules = (
-        Rule(LinkExtractor(allow='^/index\.php.+pages=(1|2|3|4|5|6|7|8|9|10)$', ), follow=True),
-        Rule(LinkExtractor(allow='.+-\d+\.html$', ), follow=True),
-        Rule(LinkExtractor(allow='download\.php.+\.torrent$', ), follow=False, callback='handle_item'),
+        Rule(LinkExtractor(allow='/new\?page=(1|2|3|4|5|6|7|8|9|10)$', ), follow=True),
+        Rule(LinkExtractor(allow='/torrent/.+\.torrent$', ), follow=True, callback='handle_item'),
     )
 
     deep_rules = (
-        Rule(LinkExtractor(allow='^/index\.php.+pages=\d+$', ), follow=True),
-        Rule(LinkExtractor(allow='.+-\d+\.html$', ), follow=True),
-        Rule(LinkExtractor(allow='download\.php.+\.torrent$', ), follow=False, callback='handle_item'),
+        Rule(LinkExtractor(allow='/actress/\?page=\d+', ), follow=True),
+        Rule(LinkExtractor(allow='/actress/[^/]+', ), follow=True),
+        Rule(LinkExtractor(allow='/torrent/.+\.torrent$', ), follow=True, callback='handle_item'),
     )
 
     def handle_item(self, response: Response) -> TorrentFileItem:
