6c402936e5eb0169d1bec3f76c2451e38aac4081
AlloVince <allo.vince@gmail.com>
2026-09-02 23:50:40 +0800

    fix(spider): repair shanaproject pagination

--- evascrapy/spiders/shanaproject_spider.py
+++ evascrapy/spiders/shanaproject_spider.py
@@ -3,28 +3,34 @@
 import math
 import time
 
-from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.http import Response
+from scrapy.spiders import Rule
 
 from evascrapy.base_spider import BaseSpider
 from evascrapy.items import TorrentFileItem
 
 
 class ShanaProjectSpider(BaseSpider):
-    version = '1.0.0'
+    version = '1.1.0'
     name = 'shanaproject'
     allowed_domains = ['www.shanaproject.com', 'media.shanaproject.com']
     start_urls = ['https://www.shanaproject.com/']
     deep_start_urls = ['https://www.shanaproject.com/']
     rules = (
-        Rule(LinkExtractor(allow=r'/\?page=\d+$'), follow=True),
-        Rule(LinkExtractor(
-            allow=r'/download/\d+/$'
-        ), follow=False, callback='handle_torrent'),
+        # Shana's catalogue uses /anime/<page>/ for pagination.
+        Rule(LinkExtractor(allow=r'/anime/[1-9]\d*/$'), follow=True),
+        Rule(LinkExtractor(allow=r'/download/\d+/$'), follow=False,
+             callback='handle_torrent'),
     )
     deep_rules = rules
-    custom_settings = {'CLOSESPIDER_ITEMCOUNT': 0}
+    custom_settings = {
+        'CLOSESPIDER_ITEMCOUNT': 0,
+        # The media host intermittently resets concurrent connections.
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 2,
+        'DOWNLOAD_DELAY': 0.5,
+        'RETRY_TIMES': 5,
+    }
 
     def handle_torrent(self, response: Response) -> TorrentFileItem:
         return TorrentFileItem(
